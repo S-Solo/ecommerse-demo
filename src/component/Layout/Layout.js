9d98e85a6ce5ffb05f601d985bb7c18b105534b9
Sokrat Poghosyan <sorkat.poghosyan@gmail.com>
2021-01-11 20:25:16 +0400

    Layout finish

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 
 import './Layout.scss';
 
 const Layout = ({ children }) => {
     return (
         <div className="app-layout">
-            <main>
+            <main className="app-layout-content">
                 {children}
             </main>
+            <Footer />
         </div>
     )
 }
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -3,7 +3,10 @@ import React, { useState } from 'react';
 import './Header.scss';
 
 const Header = () => {
-    const [userState, setUserState] = useState(null);
+
+
+
+
 
 
     return (
