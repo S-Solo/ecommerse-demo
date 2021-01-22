791e4b417ef45d3adc2223203154e83ff12afd12
Sokrat Poghosyan <sorkat.poghosyan@gmail.com>
2021-01-22 18:39:52 +0400

    Changed Auth to class

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,43 +2,12 @@ import React, { Component } from 'react';
 
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
-
-// const Auth = () => {
-//     const [isLogin, setIsLogin] = useState(true); // React hook v16.8
-
-//     const toggleView = () => {
-//         setIsLogin(!isLogin);
-//     }
-
-//     return (
-//         <div>
-//             <span onClick={toggleView}>Go to {isLogin ? 'Signup' : 'Login'}</span>
-//             {isLogin ?
-//                 <Login />
-//                 :
-//                 <Signup />
-//             }
-//         </div>
-//     )
-// }
-
-// export default Auth;
-
-
 class AuthClass extends Component {
     constructor() {
         super();
-        // this.toggleView = this.toggleView.bind(this);
-
         this.state = {
             isLogin: true
         }
-    }
-
-    toggleView() {
-        this.setState({
-            isLogin: !this.state.isLogin
-        })
     }
 
     toggleView = () => {
